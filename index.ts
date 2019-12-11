25fcd61bddc7905ae91ae6dd02726ad3329c29ab
Iku Iwasa <iku.iwasa@gmail.com>
2019-12-12 00:08:12 +0900

    Support tags with pattern like X.Y.Z, vX.Y.Z

--- index.ts
+++ index.ts
@@ -45,15 +45,71 @@ async function getLatestRelease(owner: string, project: string) {
   return null;
 }
 
+async function getTags(owner: string, project: string, after: string, num: number = 10) {
+  const { repository } = await graphql(`query tags($owner: String!, $project: String!, $after: String, $num: Int!)
+{
+  repository(owner: $owner, name: $project) {
+    refs(first: $num, after: $after, refPrefix: "refs/tags/", orderBy: { direction: DESC, field: TAG_COMMIT_DATE }) {
+      nodes {
+        name
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+}`, {
+  owner,
+  project,
+  after,
+  num,
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`
+  }
+});
+  return repository.refs;
+}
+
+async function getMatchedTag(owner: string, project: string) {
+  let hasNext = true;
+  let after = null;
+  while (hasNext) {
+    const refs = await getTags(owner, project, after);
+    for (let node of refs.nodes.reverse()) {
+      if (/^v?[0-9.]+$/.test(node.name)) {
+        return node;
+      }
+    }
+    after = refs.pageInfo.endCursor;
+    hasNext = refs.pageInfo.hasNextPage;
+  }
+  return null;
+}
+
+
 async function main() {
   for (let i = 2; i < process.argv.length; i++) {
-    const repo = process.argv[i];
+    let repo = process.argv[i];
+    const tag = /tag:(.*)/.exec(repo);
+    if (tag) {
+      repo = tag[1];
+    }
     const [owner, project] = repo.split('/');
-    const release = await getLatestRelease(owner, project);
-    if (release) {
-      console.log(`${repo}: ${release.tagName}`);
+    if (tag) {
+      const tag = await getMatchedTag(owner, project);
+      if (tag) {
+        console.log(`${repo}: ${tag.name}`);
+      } else {
+        console.log(`${repo}: No tag found!`);
+      }
     } else {
-      console.log(`${repo}: No release found!`);
+      const release = await getLatestRelease(owner, project);
+      if (release) {
+        console.log(`${repo}: ${release.tagName}`);
+      } else {
+        console.log(`${repo}: No release found!`);
+      }
     }
   }
 }
